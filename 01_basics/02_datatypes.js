bc29ea2cf7bea0c533e5dfb6f0bc5abed4011754
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-03 14:42:26 +0500

    Learned datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+//  node 01_basics/02_datatypes.js
+
+"use strict"; //Treat all JS code as a newer version
+
+// alert(3+3);  //We are using node not browser so it will not work
+
+let age= 18;
+let name= "Amamr";
+let value =true;
+let reponse= undefined;
+let score = null;
+
+console.log(typeof null);  //datatype of null is object resaon is strange behaviour of JS or Problem in JS many opinions about it,
