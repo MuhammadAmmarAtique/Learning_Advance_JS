3f1e8ef7063c54f9d4432f8e42ccc448e8bd6c31
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-03 19:45:23 +0500

    Minor changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,12 @@
+//  node 01_basics/01_variables.js
+
 const accountId = 1234;
-let accountEmail = "Ammar123@gmail.com";
+let accountEmail = "Ammar123@gmail.com"; //Always use let
 var accountPassword = 876134;
 accountcity = "Haripur";  //Bad way of declaring a variable
 let accountState;
 
-// accountId=4321;
+// accountId=4321; //cant change constant
 accountEmail="Rafeh1234@gmail.com";
 
 console.table([accountId,accountEmail,accountPassword,accountcity,accountState]);
--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,10 +4,13 @@
 
 // alert(3+3);  //We are using node not browser so it will not work
 
+// Below are list of different datatypes
 let age= 18;
 let name= "Ammar";
 let value =true;
 let reponse= undefined;
 let score = null;
 
-console.log(typeof null);  //datatype of null is object resaon behind this is the strange behaviour of JS or it's a  Problem in JS, There are many opinions about it,
+console.log(typeof null);  
+
+//datatype of null is object resaon behind this is the strange behaviour of JS or it's a  Problem in JS, There are many opinions about it,
