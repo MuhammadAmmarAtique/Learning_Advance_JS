bfa6e1a13a3e3a825d09647e4bd22793ba9a3cb4
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-03 13:50:42 +0500

    Learned about variables and constants

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1234;
+let accountEmail = "Ammar123@gmail.com";
+var accountPassword = 876134;
+accountcity = "Haripur";  //Bad way of declaring a variable
+let accountState;
+
+// accountId=4321;
+accountEmail="Rafeh1234@gmail.com";
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState]);
