53d24e16f0189ab183ee279485077506a70a3dcb
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-03 19:42:42 +0500

    learned strange behaviour of conversion in js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+// node 01_basics/03_conversionOperation
+
+let score = "22aa";
+let  valueInNumber = Number(score); //converting string into number
+console.log(typeof(valueInNumber)); //it will give wrong/unexpected result
+console.log(valueInNumber); //so do this.
+
+
+
+
