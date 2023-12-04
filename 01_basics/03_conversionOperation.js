5ef1bd1324d862ec52e4b0fab86e9df8b82911e1
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-04 18:59:39 +0500

    Learned about string to number tricky conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -36,5 +36,48 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
  let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//////////////////////////////////OPEARATIONS dURING STRING TO NUMBER////////////////////////////////////////////////
+                              //  "or" NUMBER TO STRING CONVERSIONS 
+
+
+let newvalue = 3
+let negValue = -newvalue
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);
+// console.log("3"+"4"+1+2);
+
+
+// These type of question may be asked in mcqs, interview but in roduction they are useless.
