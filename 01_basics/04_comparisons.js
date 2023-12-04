661cd9b77bdb3a736c44894a464297d6c0f8d3a7
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-04 23:01:32 +0500

    Learned about comparisons in js

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,8 @@
+// 04) Comparisons of Datatypes
+// node 01_basics/04_comparisons.js
+
+// When comparing same datatypes there is no issue, but when we go in different datatypes then real problem may come.
+
+// here strings are converted into numbers
+console.log("2">1);
+console.log("02">1);
