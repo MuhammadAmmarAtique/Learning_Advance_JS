46df03e088e4743cb3d5aaa09151addf189f0604
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-06 22:05:15 +0500

    revised strings

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,18 @@
+// Strings ins JS
+// node 01_basics/07_strings.js
+
+let name = new String("Ammar Atiq");
+// console.log(name.length);
+
+
+// .at()
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+let index = 8;
+// console.log(`An index of ${index} returns the character ${sentence.at(index)}`);
+
+//.charat()
+// console.log(sentence.charAt(4));
+
+//.indexOf()
+const paragraph = "I think Ruth's dog is cuter than your dog!";
+console.log(paragraph.indexOf('t'));
