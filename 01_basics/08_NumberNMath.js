dca22a48af23debf323dfaf09f751b4dba664b97
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-08 16:15:37 +0500

    minor changes

--- 01_basics/08_NumberNMath.js
+++ 01_basics/08_NumberNMath.js
@@ -19,22 +19,17 @@ const hundreds = 1000000
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-console.log(Math);
+// console.log(Math);
 // console.log(Math.abs(-4123));
 // console.log(Math.round(4.9));
 // console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9));
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
-
 // console.log(Math.random());
-console.log(Math.floor((Math.random()*19) + 1));
-// console.log(Math.floor(Math.random()*10));
 
-const max = 20
-const min = 10
 
-// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
 
 
 
