43c68a4e4b8a5befb05266727b2991d4604d40c0
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-08 14:28:27 +0500

    minor chnages

--- 01_basics/08_NumberNMath.js
+++ 01_basics/08_NumberNMath.js
@@ -28,11 +28,11 @@ console.log(Math);
 // console.log(Math.max(4, 3, 6, 8));
 
 // console.log(Math.random());
-// console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*19) + 1));
 // console.log(Math.floor(Math.random()*10));
 
-const min = 10
 const max = 20
+const min = 10
 
 // console.log(Math.floor(Math.random() * (max - min + 1)) + min)
 
