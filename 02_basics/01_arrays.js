57e4ca66b0f5e1f2cc67ece771cd63920410f6e5
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-09 11:34:43 +0500

    minor changes

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,18 @@
+//arrrays
+
+let arr1 = ["mama","papa"];
+let arr2 = ['rafeh',`ammar`,"moiz"];
+
+// arr1.push(arr2);
+// console.log(arr1[2]);
+
+
+// let arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+
+//spread operator 
+// (ya opearor dono arrays ko spread karaay ga aur phir haam isko join kardeingy aik new array mai)
+
+let arr3= [...arr1 , ...arr2];
+console.log(arr3);
