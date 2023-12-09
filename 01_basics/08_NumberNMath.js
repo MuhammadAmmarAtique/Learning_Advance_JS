b16aa6a267451f0297f7cf00382a10e72709edc8
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-09 14:55:28 +0500

    learned mnore about math object

--- 01_basics/08_NumberNMath.js
+++ 01_basics/08_NumberNMath.js
@@ -26,7 +26,15 @@ const hundreds = 1000000
 // console.log(Math.floor(4.9));
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
-// console.log(Math.random());
+
+// imp methods
+
+// console.log(Math.random());  //It will generate number between 0 and 1
+// console.log(Math.random()*10);   //It will generate number between 0 and 1 wwhich is then multiplied by 10 but it still can generate number of 0
+
+// console.log(Math.random()*10 +1);   // Now Adding +1 in end will resolve this issue
+// console.log(Math.floor((Math.random()*10) +1));  //use math.floor to end decimals                      
+
 
 
 
