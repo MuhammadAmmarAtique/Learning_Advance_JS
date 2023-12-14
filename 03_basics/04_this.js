9efef8cd1f8beb8ae5d36770b911cde829a87f24
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-14 16:06:01 +0500

    Learned about this keyword

---
+++ 03_basics/04_this.js
@@ -0,0 +1,20 @@
+//Using This keyword
+let user = {
+    username: "Ammar",
+    age:21,
+    PricePaid: 1299,
+
+    greeting:function() {
+        console.log(`Hey dear user your name is ${this.username},Your age is ${this.age} and you paid ${this.PricePaid} for our website registration`);
+    }
+}
+
+user.greeting();
+
+//New user comes in
+user.username= "Rafeh";
+user.age=24;
+user.PricePaid= 1000;
+user.greeting();
+
+
