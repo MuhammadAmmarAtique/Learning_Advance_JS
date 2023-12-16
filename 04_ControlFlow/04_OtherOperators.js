690bedf3ab396799db95bc0aeafe11766b3d7c69
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-16 19:08:52 +0500

    learned about opearators

---
+++ 04_ControlFlow/04_OtherOperators.js
@@ -0,0 +1,19 @@
+// 1)Nullish Coalescing Operator
+
+let val1;
+// val1 = 23 ?? null 
+// val1 = undefined ?? 22
+
+// val1 = 5 ?? 10
+// val1 = 15 ?? 10 
+// val1 = 25 ?? 10 ?? 27 ??56
+// console.log(val1);
+
+// 2)Ternary Operator
+// condition? true :false
+
+let number =21;
+
+number==22 ? console.log("yes condition is true"):console.log("Sorry condition is false")
+
+
