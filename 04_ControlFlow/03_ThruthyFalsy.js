bff2eb3eb4c609d9880ee5ceb18fe27d99f9bb81
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-16 16:03:05 +0500

    Learned about thruthy falsy

---
+++ 04_ControlFlow/03_ThruthyFalsy.js
@@ -0,0 +1,13 @@
+// Thruthy Falsy Values
+
+// let userEmail = "ammar@email.com"
+// if (userEmail) 
+// {
+//     console.log("Thanks for proving email, we will keep you updated!");
+// } else
+// {
+//     console.log("Please enter a valid email");
+// }
+
+
+
