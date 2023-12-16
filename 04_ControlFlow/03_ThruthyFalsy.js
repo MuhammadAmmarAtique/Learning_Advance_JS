2771a28949bbb904f8e66248a3818cbaed22d485
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-16 16:47:17 +0500

    Checking emty objects/Arrays

--- 04_ControlFlow/03_ThruthyFalsy.js
+++ 04_ControlFlow/03_ThruthyFalsy.js
@@ -1,4 +1,4 @@
-// Thruthy Falsy Values
+// 1)Thruthy Falsy Values
 
 // let userEmail = "ammar@email.com"
 // if (userEmail) 
@@ -8,6 +8,35 @@
 // {
 //     console.log("Please enter a valid email");
 // }
+
+
+// 2)Checking/Validating empty Objects and Arrays
+
+//a-Array
+
+let userData = [];
+
+if (userData.length === 0)
+ {
+  console.log("Its empty Array"); 
+ } else {
+  console.log("All good (array)");     
+}
+
+//b-Object
+
+let AdminManagerData = {};
+
+// console.log(Object.keys(AdminManagerData));
+// console.log(Object.keys(AdminManagerData).length);
+
+if (Object.keys(AdminManagerData).length ===0)
+ {
+  console.log("Its empty object"); 
+ } else {
+  console.log("All good (object)");     
+}
+ 
 
 
 
