c85bee1f0e116fe7eb1fba5f8ae8e65cae720058
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-16 12:44:00 +0500

    Learned about control flows

---
+++ 04_ControlFlow/01_.js
@@ -0,0 +1,40 @@
+//1) if else ladder
+
+// let temp = 20;
+
+// if (temp < 20) {
+//   console.log("less then 20'C");
+// } 
+
+// else if (temp == 20) {
+//   console.log("Temp equal to 20'C");   //"if else ladder"  mai sirf aik piece of code execute hoga,
+//                                         // jiski pahli condition true hogee
+// }
+ 
+// else if (temp < 40) {
+//   console.log("Temp less then 40'C");    
+// }
+
+// else {
+//   console.log("Sorry all conditions are false");
+// }
+
+//2) Using AND,OR Operators
+
+let num1 =2;
+let num2 =4;
+let num3 =6;
+let num4 =8;
+let num5 =10;
+
+if (num1==2 && num2==4  &&  num3==6 && num4==8 && num5==10) {
+    console.log("Yes bro numbers are equal");
+}
+
+if (num1==3 || num2==1  ||  num3==0 || num4==1 || num5==10) {
+    console.log("Yes bahi numbers are equal");
+}
+
+
+//Imp Note: We can use multiple AND,OR Operators.
+
