c654191238581c3f5c8062c76a287556ed24922b
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-17 15:17:03 +0500

    Learned about while and dowhile loop

---
+++ 05_Loops/02_.js
@@ -0,0 +1,25 @@
+//While Dowhile loop
+
+// a)While
+
+// let i=0;
+// while (i<5) {
+//     console.log(i);
+//     i++;   
+// }
+
+let myArray =[1,2,3,4]
+let i=0;
+while (i<myArray.length) {
+    console.log(myArray[i]);
+    i++;
+}
+
+
+// b)Do While
+
+// let j=11;
+// do {
+//     console.log(j);
+//     j++;
+// } while (j<10);
