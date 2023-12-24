b9b4d7315533cd1716b5a51e46e98d7357702c0b
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-24 19:30:02 +0500

    Again did project 1

--- 07_Dom_Projects/Project-01/index.js
+++ 07_Dom_Projects/Project-01/index.js
@@ -1,10 +1,24 @@
-let Allboxes =document.querySelectorAll('.box');
+// 1)Solution 1
 
-Allboxes.forEach((div)=>{
-         div.addEventListener('click',()=>{
+// let Allboxes =document.querySelectorAll('.box');
 
-            let color = div.getAttribute('data-color');
-            document.body.style.backgroundColor = color;
+// Allboxes.forEach((div)=>{
+//          div.addEventListener('click',()=>{
 
-         })
+//             let color = div.getAttribute('data-color');
+//             document.body.style.backgroundColor = color;
+
+//          })
+// })
+
+// 2)Solution 2
+
+let Allboxes=document.querySelectorAll('.box');
+Allboxes.forEach((box)=>{
+    box.addEventListener("click",(e)=>{
+        // console.log(e.target.id);
+        document.body.style.backgroundColor = e.target.id;
+        
+    })
 })
+
