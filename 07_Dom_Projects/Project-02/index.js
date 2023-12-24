33e9ac86122de40430cb24ad37f4b1454ae93490
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-25 00:47:54 +0500

    Improved logic

--- 07_Dom_Projects/Project-02/index.js
+++ 07_Dom_Projects/Project-02/index.js
@@ -1,58 +1,46 @@
-let height = document.querySelector('#height');
-let weight = document.querySelector('#weight');
-let result = document.querySelector('#results');
-let myform = document.querySelector('#myForm');
-
-let button = document.querySelector('#button');
-button.disabled=true;
-
-// Enabling submit button only if there is data in both input fields
-let inputFields = document.querySelectorAll("input");
-inputFields.forEach((element)=>{
-element.onkeyup= ()=>
-{
-    if(height.value.length>=1 && weight.value.length>=1 )
-    {
-    button.disabled=false;
-    }
-    else
-    {
-        button.disabled=true;
-    }
-}
-}) 
+let myform = document.querySelector("#myForm");
 
 // Main Logic of Calculating BMI (when we submit form data goes to backend)
-myform.addEventListener("submit",(e)=>{
-    e.preventDefault();
+  myform.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    // Calculating user's BMI only if user has entered "number" (its actually string  number)
-    if (!isNaN(height.value) && !isNaN(weight.value)) {
+  // Converting string into number
+  let height = parseFloat(document.querySelector("#height").value);
+  let weight = parseFloat(document.querySelector("#weight").value);
+  let result = document.querySelector("#results");
 
-        // Converting string into number
-          let heightinNumber= parseFloat(height.value);
-          let widthinNumber=  parseFloat(weight.value);
-
-        let Calculated_BMI = widthinNumber / ((heightinNumber / 100) ** 2);
-        let RoundedVal = Calculated_BMI.toFixed(2);
-
-        result.textContent= `Your BMI is ${RoundedVal}`;
-    
-        //When result is shown again disabling button 
-       button.disabled=true;
-    
-       //Emptying both input fields in end   
-       height.value="";
-       weight.value="";
+  //   checking if user entered valid values in height and weight section
+    if (height === "" || height < 0 || isNaN(height)) {
+    result.textContent = "Please enter a valid height";
     } 
+    else if (weight === "" || weight < 0 || isNaN(weight)) {
+    result.textContent = "Please enter a valid Width";
+    } 
+    // Calculating BMI and printing it  
     else {
-        result.textContent = "Incorrect input, Please enter again";    
-        
-        //When result is shown again disabling button 
-       button.disabled=true;
-    
-       //Emptying both input fields in end   
-       height.value="";
-       weight.value="";
+
+    let Calculated_BMI = (weight / (height / 100) ** 2).toFixed(2);
+
+    let AdditionalMsg;
+    switch (true) {
+        case Calculated_BMI < 18.6:
+            AdditionalMsg = "Under Weight";
+            break;
+        case Calculated_BMI >= 18.6 && Calculated_BMI <= 24.9:
+            AdditionalMsg = "Normal Range";
+            break;
+        case Calculated_BMI > 24.9:
+            AdditionalMsg = "Overweight";
+            break;
+        default:
+            AdditionalMsg = "Unknown BMI Range";
     }
-})
+   
+
+    result.textContent = `Your BMI is ${Calculated_BMI} (${AdditionalMsg})`;
+
+    //Emptying both input fields in end
+    document.querySelector("#height").value = "";
+    document.querySelector("#weight").value = "";
+  }
+});
