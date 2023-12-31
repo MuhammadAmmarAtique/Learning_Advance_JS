0e033eaec6c6e981281121db12e117bc84dd9776
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2023-12-31 20:05:42 +0500

    Learned about prototype

---
+++ 12_Oops/protoype.js
@@ -0,0 +1,24 @@
+//Prototype
+
+function UserDetails(username, age, salary) {
+    this.username =username;
+    this.age =age;
+    this.salary =salary;
+    
+    return this;
+}
+
+//Adding a prototype in above function
+UserDetails.prototype.increment = function(){
+    this.age++;
+    console.log(this.age);
+}
+
+let user1 = new UserDetails("Ammar",22,10000)
+let user2 = new UserDetails("Rafeh",24,20000)
+
+console.log(user1);
+console.log(user2);
+
+//calling that function which is added in prototype
+user1.increment()
