d7ce19280f8640160dd5d9bb37ad92cb4103c128
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-01-02 10:16:18 +0500

    Learned call ( ) in js

---
+++ 12_Oops/05_Call.js
@@ -0,0 +1,31 @@
+//.Call() in JS
+
+function UserInformation(Username,email,password) {
+
+    // SetUserName(Username);
+
+    // Waisaay bhi function (UserName) call tooh ho raha hai lakin wo values apnay "this" mai store kar raha hai 
+    // aur execute ho kar khatam ho raha hai tooh is case mai haam .call() use karengaay aur kaheengaay kaay values
+    // UserInformation ka this mai store karoo
+
+    SetUserName.call(this,Username); //yahan "this" UserInfromation ka "this" hai
+    this.email = email;
+    this.password = password;
+
+    return this;
+}
+
+function SetUserName(Username) {
+    this.name = Username;
+    console.log("SetUserName function called");
+
+    return this;
+}
+
+
+let ammar = new UserInformation("Ammar","ammar123@gmail.com",123)
+console.log(ammar);
+
+let danish =  new UserInformation("Danish","danish123@gmail.com",123)
+console.log(danish);
+
